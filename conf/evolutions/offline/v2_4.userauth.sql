72f77a4ec1decd881248eccf93f5f12c66daab7f
Ian Holsman <Ian@Holsman.com.au>
2017-05-08 08:11:11 -0500

    Case Sensitivity

--- conf/evolutions/offline/v2_4.userauth.sql
+++ conf/evolutions/offline/v2_4.userauth.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `authpermission`
+ALTER TABLE `AuthPermission`
   ADD COLUMN `description` TEXT NULL AFTER `permission`;
-ALTER TABLE `authrole`
+ALTER TABLE `AuthRole`
   ADD COLUMN `description` TEXT NULL AFTER `role`;
-ALTER TABLE `authrole`
+ALTER TABLE `AuthRole`
   ADD COLUMN `isAdmin` BIT NOT NULL DEFAULT b'0' AFTER `description`;
