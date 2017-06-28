8c9fca9090b230660a77441d882c484a8f1da3dd
Ian Holsman <Ian@Holsman.com.au>
2017-06-28 09:14:36 -0500

    add permision to show date panel

--- conf/create/meta.offline.sql
+++ conf/create/meta.offline.sql
@@ -1,7 +1,11 @@
 -- Offline DB
 INSERT INTO `AuthRole` (`role`, `description`, `isAdmin`) VALUES ('HR', 'Human Resources', b'0');
 INSERT INTO `AuthRole` (`role`, `description`, `isAdmin`) VALUES ('AwardCommittee', 'Members of the Award Committee', b'0');
+INSERT INTO `AuthRole` (`role`, `description`, `isAdmin`) VALUES ('EPMO', 'EPMO', b'0');
 INSERT INTO `AuthPermission` (`permission`, `description`) VALUES ('AuthorizeAwardsHR', 'used to authorize awards at the HR level');
 INSERT INTO `AuthPermission` (`permission`, `description`) VALUES ('AuthorizeAwards', 'After HR Authorizes, allows award to be \'public\'');
+INSERT INTO `AuthPermission` (`permission`, `description`) VALUES ('permTeamHireDate', 'Display hire date panel on Resource pages');
+
 INSERT INTO `AuthRolePermission` (`permissionID`, `roleID`) VALUES (2, 2);
 INSERT INTO `AuthRolePermission` (`permissionID`, `roleID`) VALUES (1, 1);
+INSERT INTO `AuthRolePermission` (`permissionID`, `roleID`) VALUES (3, 3);
